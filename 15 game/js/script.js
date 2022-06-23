c90cad4cd0836b479fd016a31b1cff9fb3252596
SneaKyLight <hugofouche499@gmail.com>
2022-06-23 14:31:14 +0200

    15 game final version

--- 15 game/js/script.js
+++ 15 game/js/script.js
@@ -1,7 +1,8 @@
 const vm = Vue.createApp({
     data() {
         return {
-            message: ""
+            message: "",
+            timer: ""
         }
     },
     methods: {
@@ -10,6 +11,19 @@ const vm = Vue.createApp({
         },
         actualiseMessagePartieTerminee(nbCoups) {
             this.message = "Bravo, vous avez gagné en " + nbCoups + " coups.";
+        },
+        actualiseMessagePartieReset() {
+            this.message = "";
+        },
+        reroll() {
+            this.$refs.game.createItems();
+        },
+        actualiseTimer(timer) {
+            this.timer = timer;
+        },
+        resolve() {
+            console.log("Solving...");
+            this.$refs.game.resolve();
         }
     }
 })
@@ -39,16 +53,65 @@ vm.component('box-item', {
 
 vm.component('box', {
     props: ['amount'],
-    emits: ['game-finished', 'game-not-finished'],
+    emits: ['game-finished', 'game-not-finished', 'game-reset', 'timer-update'],
     data() {
         return {
             items: [],
             positionVide: 15,
             gameFinished: false,
-            nbCoups: 0
+            nbCoups: 0,
+            timer: 0,
+            interval: null
         }
     },
     methods: {
+        keyEvnt(direction) {
+            if(!this.gameFinished) {
+                let positionXvide = this.positionVide % 4;
+                let positionYvide = Math.floor(this.positionVide / 4);
+
+                let pos = 0;
+                let number = 0;
+                if(direction == "right" && positionXvide > 0) {
+                    this.items.forEach(item => {
+                        if(item.position == this.positionVide-1) {
+                            pos = item.position;
+                            number = item.number;
+                        }
+                    });
+
+                    this.moveItem(number, pos);
+                }
+                else if(direction == "left" && positionXvide < 3) {
+                    this.items.forEach(item => {
+                        if(item.position == this.positionVide+1) {
+                            pos = item.position;
+                            number = item.number;
+                        }
+                    });
+
+                    this.moveItem(number, pos);
+                } else if(direction == "down" && positionYvide > 0) {
+                    this.items.forEach(item => {
+                        if(item.position == this.positionVide-4) {
+                            pos = item.position;
+                            number = item.number;
+                        }
+                    });
+
+                    this.moveItem(number, pos);
+                } else if(direction == "up" && positionYvide < 3) {
+                    this.items.forEach(item => {
+                        if(item.position == this.positionVide+4) {
+                            pos = item.position;
+                            number = item.number;
+                        }
+                    });
+
+                    this.moveItem(number, pos);
+                }
+            }
+        },
         moveItem(number, position) {
             //Move l'item dans la bonne position si possible
             posX = position % 4;
@@ -69,6 +132,7 @@ vm.component('box', {
             
             if(this.verifItems()) {
                 this.gameFinished = true;
+                clearInterval(this.interval);
                 this.$emit('game-finished', this.nbCoups);
             } else {
                 this.$emit('game-not-finished', this.nbCoups);
@@ -87,6 +151,10 @@ vm.component('box', {
             //Crée les items
             let positionsUtilises = [];
             this.items = [];
+            this.$emit('game-reset');
+            this.timer = 0;
+            this.$emit('timer-update', this.timer);
+            this.gameFinished = false;
             for(let i = 1;i <= parseInt(this.amount);i++) {
                 let position = Math.floor(Math.random() * (parseInt(this.amount)+1));
                 while(positionsUtilises.includes(position)) {
@@ -103,26 +171,35 @@ vm.component('box', {
             }
             if(!this.verifSolvability()) {
                 this.createItems();
+            } else {
+                if(this.interval != null) {
+                    clearInterval(this.interval);
+                }
+                this.interval = setInterval(this.updateTimer, 1000);
             }
         },
         verifSolvability() {
             //Verifie si le jeu est solvable
             //On commence par chercher la parité de la position du vide
-            let pariteVide = this.positionVide % 2;
+            let posVideX = this.positionVide % 4;
+            let posVideY = Math.floor(this.positionVide / 4);
+            let pariteVide = posVideX % 2 + posVideY % 2;
             //On cherche ensuite la partié du nombre de permutations pour gagner
             let nbPermutations = 0;
             let copiesItems = [];
             this.items.forEach(item => {
                 copiesItems.push({number: item.number, position: item.position});
             });
-            for(let i = 0; i < copiesItems.length; i++) {
-                nbPermutations += Math.abs(copiesItems[i].position - i);
-                //On cherche l'item qui a la position i
-                let item = copiesItems.find(item => item.position == i);
-                if(item != undefined) {
-                    item.position = copiesItems[i].position;
+            let copiesFinales = copiesItems.slice(0, this.positionVide)
+            copiesFinales.push(null);
+            copiesFinales = copiesFinales.concat(copiesItems.slice(this.positionVide));
+            //On compte les permutations
+            for(let i = 0;i < copiesItems.length-1;i++) {
+                for(let j = i+1;j < copiesItems.length;j++) {
+                    if(copiesFinales[j] != null && copiesFinales[i] != null && copiesFinales[i].number > copiesFinales[j].number) {
+                        nbPermutations++;
+                    }
                 }
-                copiesItems[i].position = i;
             }
             let paritePermutations = nbPermutations % 2;
             //Si les deux parités sont identiques, le jeu est solvable
@@ -130,16 +207,31 @@ vm.component('box', {
                 return true;
             }
             return false;
+        },
+        updateTimer() {
+            this.timer++;
+            this.$emit('timer-update', this.timer);
         }
     },
     mounted() {
         this.createItems();
+        window.addEventListener('keydown', (e) => {
+            if(e.key == "ArrowRight") {
+                this.keyEvnt('right');
+            } else if(e.key == "ArrowLeft") {
+                this.keyEvnt('left');
+            } else if(e.key == "ArrowUp") {
+                this.keyEvnt('up');
+            } else if(e.key == "ArrowDown") {
+                this.keyEvnt('down');
+            }
+        });
     },
     template: `
-        <section class="game-container">
+        <section class="game-container" :class="{win: gameFinished}">
             <box-item v-for="item in items" :key="item.number" :number="item.number" :position="item.position" @box-item-double-click="moveItem" :locked="gameFinished"></box-item>
         </section>
     `
 });
 
-vm.mount('#game-container');
+vm.mount('body');
